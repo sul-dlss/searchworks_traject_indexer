e6960654844b4b074c3f299573034a2cc008fe3f
Chris Beer <cabeer@stanford.edu>
2018-08-20 07:34:03 -0700

    genre_ssim was slicing too much data from the 008/006

--- lib/traject/config/sirsi_config.rb
+++ lib/traject/config/sirsi_config.rb
@@ -1304,10 +1304,10 @@ end
 # **/
 to_field 'genre_ssim' do |record, accumulator|
   if record['008'] && record['008'].value.length >= 28
-    accumulator << 'Technical report' if (record.leader[6] == 'a' || record.leader[6] == 't') && record['008'].value[24..28] =~ /t/
+    accumulator << 'Technical report' if (record.leader[6] == 'a' || record.leader[6] == 't') && record['008'].value[24..27] =~ /t/
   elsif record['027'] || record['088']
     accumulator << 'Technical report'
-  elsif record['006'] && (record['006'].value[0] == 'a' || record['006'].value[0] == 't') && record['006'].value[7..11] =~ /t/
+  elsif record['006'] && (record['006'].value[0] == 'a' || record['006'].value[0] == 't') && record['006'].value[7..10] =~ /t/
     accumulator << 'Technical report'
   end
 end
