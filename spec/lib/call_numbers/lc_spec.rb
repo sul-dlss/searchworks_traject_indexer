3caa67e8212789570705a14bd729b65caa1e3bff
Chris Beer <cabeer@stanford.edu>
2018-08-23 17:31:35 -0700

    Use described_class in our LC tests

--- spec/lib/call_numbers/lc_spec.rb
+++ spec/lib/call_numbers/lc_spec.rb
@@ -1,98 +1,99 @@
 require 'call_numbers/lc'
+
 describe CallNumbers::LC do
 
   describe 'standard numbers' do
     it 'handles 1 - 3 class letters' do
-      expect(CallNumbers::LC.new('P123.23 .M23 2002').klass).to eq 'P'
-      expect(CallNumbers::LC.new('PS123.23 .M23 2002').klass).to eq 'PS'
-      expect(CallNumbers::LC.new('PSX123.23 .M23 2002').klass).to eq 'PSX'
+      expect(described_class.new('P123.23 .M23 2002').klass).to eq 'P'
+      expect(described_class.new('PS123.23 .M23 2002').klass).to eq 'PS'
+      expect(described_class.new('PSX123.23 .M23 2002').klass).to eq 'PSX'
     end
 
     it 'parses the class number and decimal' do
-      expect(CallNumbers::LC.new('P123.23 .M23 2002').klass_number).to eq '123'
-      expect(CallNumbers::LC.new('P123.23 .M23 2002').klass_decimal).to eq '.23'
+      expect(described_class.new('P123.23 .M23 2002').klass_number).to eq '123'
+      expect(described_class.new('P123.23 .M23 2002').klass_decimal).to eq '.23'
 
-      expect(CallNumbers::LC.new('P123 .M23 2002').klass_number).to eq '123'
-      expect(CallNumbers::LC.new('P123 .M23 2002').klass_decimal).to be_nil
+      expect(described_class.new('P123 .M23 2002').klass_number).to eq '123'
+      expect(described_class.new('P123 .M23 2002').klass_decimal).to be_nil
     end
 
     describe 'doons (dates or other numbers)' do
       it 'parses a doon after the class number/decimal and before the 1st cutter' do
-        expect(CallNumbers::LC.new('P123.23 2012 .M23 2002 .M45 V.1').doon1).to match(/^2012/)
+        expect(described_class.new('P123.23 2012 .M23 2002 .M45 V.1').doon1).to match(/^2012/)
       end
 
       it 'parses a doon after the 1st cutter and before other cutters' do
-        expect(CallNumbers::LC.new('P123.23 2012 .M23 2002 .M45 V.1').doon2).to match(/^2002/)
+        expect(described_class.new('P123.23 2012 .M23 2002 .M45 V.1').doon2).to match(/^2002/)
       end
 
       it 'parses a doon after the 2nd cutter and before other cutters' do
-        expect(CallNumbers::LC.new('G4362 .L3 .E63 1997 .E2').doon3).to match(/^1997/)
+        expect(described_class.new('G4362 .L3 .E63 1997 .E2').doon3).to match(/^1997/)
       end
 
       it 'allows for characters after the number in the doon (e.g. 12TH)' do
-        expect(CallNumbers::LC.new('P123.23 20TH .M45 V.1').doon1).to match(/^20TH/)
+        expect(described_class.new('P123.23 20TH .M45 V.1').doon1).to match(/^20TH/)
       end
     end
 
     describe 'cutters' do
       it 'handles 3 possible cutters' do
-        expect(CallNumbers::LC.new('P123.23 .M23 V.1 2002').cutter1).to eq '.M23'
-        expect(CallNumbers::LC.new('P123.23 .M23 V.1 2002').cutter2).to be_nil
-        expect(CallNumbers::LC.new('P123.23 .M23 V.1 2002').cutter3).to be_nil
+        expect(described_class.new('P123.23 .M23 V.1 2002').cutter1).to eq '.M23'
+        expect(described_class.new('P123.23 .M23 V.1 2002').cutter2).to be_nil
+        expect(described_class.new('P123.23 .M23 V.1 2002').cutter3).to be_nil
 
-        expect(CallNumbers::LC.new('P123.23 .M23 .M45 V.1 2002').cutter1).to eq '.M23'
-        expect(CallNumbers::LC.new('P123.23 .M23 .M45 V.1 2002').cutter2).to eq '.M45'
-        expect(CallNumbers::LC.new('P123.23 .M23 .M45 V.1 2002').cutter3).to be_nil
+        expect(described_class.new('P123.23 .M23 .M45 V.1 2002').cutter1).to eq '.M23'
+        expect(described_class.new('P123.23 .M23 .M45 V.1 2002').cutter2).to eq '.M45'
+        expect(described_class.new('P123.23 .M23 .M45 V.1 2002').cutter3).to be_nil
 
-        expect(CallNumbers::LC.new('P123.23 .M23 .M45 .S32 V.1 2002').cutter1).to eq '.M23'
-        expect(CallNumbers::LC.new('P123.23 .M23 .M45 .S32 V.1 2002').cutter2).to eq '.M45'
-        expect(CallNumbers::LC.new('P123.23 .M23 .M45 .S32 V.1 2002').cutter3).to eq '.S32'
+        expect(described_class.new('P123.23 .M23 .M45 .S32 V.1 2002').cutter1).to eq '.M23'
+        expect(described_class.new('P123.23 .M23 .M45 .S32 V.1 2002').cutter2).to eq '.M45'
+        expect(described_class.new('P123.23 .M23 .M45 .S32 V.1 2002').cutter3).to eq '.S32'
       end
 
       it 'handles multi-letter cutters' do
-        expect(CallNumbers::LC.new('P123.23 .MS23').cutter1).to eq '.MS23'
-        expect(CallNumbers::LC.new('P123.23 .MSA23').cutter1).to eq '.MSA23'
+        expect(described_class.new('P123.23 .MS23').cutter1).to eq '.MS23'
+        expect(described_class.new('P123.23 .MSA23').cutter1).to eq '.MSA23'
       end
 
       it 'handles a single letter after the cutter number' do
-        expect(CallNumbers::LC.new('P123.23 .MS23A').cutter1).to eq '.MS23A'
+        expect(described_class.new('P123.23 .MS23A').cutter1).to eq '.MS23A'
       end
 
       it 'parses cutters that do not have a space before them' do
-        expect(CallNumbers::LC.new('P123.23.M23.S32').cutter1).to eq '.M23'
-        expect(CallNumbers::LC.new('P123.23.M23.S32').cutter2).to eq '.S32'
+        expect(described_class.new('P123.23.M23.S32').cutter1).to eq '.M23'
+        expect(described_class.new('P123.23.M23.S32').cutter2).to eq '.S32'
       end
 
       it 'parses cutters with no space or period' do
-        expect(CallNumbers::LC.new('P123M23S').cutter1).to eq 'M23S'
-        expect(CallNumbers::LC.new('P123M23SL').cutter1).to eq 'M23SL'
-        expect(CallNumbers::LC.new('P123M23S32').cutter1).to eq 'M23'
-        expect(CallNumbers::LC.new('P123M23S32').cutter2).to eq 'S32'
+        expect(described_class.new('P123M23S').cutter1).to eq 'M23S'
+        expect(described_class.new('P123M23SL').cutter1).to eq 'M23SL'
+        expect(described_class.new('P123M23S32').cutter1).to eq 'M23'
+        expect(described_class.new('P123M23S32').cutter2).to eq 'S32'
       end
     end
 
     describe 'folio' do
       it 'handles folio' do
-        expect(CallNumbers::LC.new('P123M23S32 F 123').folio).to eq 'F'
+        expect(described_class.new('P123M23S32 F 123').folio).to eq 'F'
       end
 
       it 'handles a folio at the end of the string' do
-        expect(CallNumbers::LC.new('P123M23S32 F').folio).to eq 'F'
+        expect(described_class.new('P123M23S32 F').folio).to eq 'F'
       end
 
       it 'handles a flat folio at the end of the string' do
-        expect(CallNumbers::LC.new('P123M23S32 FF').folio).to eq 'FF'
+        expect(described_class.new('P123M23S32 FF').folio).to eq 'FF'
       end
 
       it 'does nothing with tons of Fs' do
-        expect(CallNumbers::LC.new('P123M23S32 FFFFF').folio).to eq nil
+        expect(described_class.new('P123M23S32 FFFFF').folio).to eq nil
       end
     end
 
     describe 'the rest of the stuff' do
       it 'puts any other content into the rest attribute' do
-        expect(CallNumbers::LC.new('P123.23 .M23 V.1 2002').rest).to eq 'V.1 2002'
-        expect(CallNumbers::LC.new('P123.23 2012 .M23 2002 .M45 V.1 2002-2012/GobbldyGoop').rest).to eq 'V.1 2002-2012/GobbldyGoop'
+        expect(described_class.new('P123.23 .M23 V.1 2002').rest).to eq 'V.1 2002'
+        expect(described_class.new('P123.23 2012 .M23 2002 .M45 V.1 2002-2012/GobbldyGoop').rest).to eq 'V.1 2002-2012/GobbldyGoop'
       end
     end
 
