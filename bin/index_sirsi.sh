53c9e9d37664a2414ffd0f2006cf62e5b08154dd
Jack Reed <phillipjreed@gmail.com>
2018-08-24 13:29:15 -0600

    add basic indexing script

---
+++ bin/index_sirsi.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Fires off indexing of all of the Marc data in $MARC_LOC
+# SOLR_URL, RESERVES_FILE, and NUM_THREADS should also be supplied
+
+# Kill all of the traject processes w/:
+# ps aux | grep traject | awk '{print $2}' | xargs kill -9
+
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+for file in $MARC_LOC/*.marc; do
+    LOG_DIR=$MARC_LOC/logs/$DATE_WITH_TIME
+    mkdir -p $LOG_DIR
+    echo -e "\nCreating indexing process for $file"
+    nohup bundle exec traject -c ../lib/traject/config/sirsi_config.rb \
+        -s solr_writer.max_skipped=-1 \
+        -s log.file=$LOG_DIR/$(basename $file).log $file \
+        &> $LOG_DIR/$(basename $file).command.log &
+    read -p 'Waiting for 10 seconds before creating the next one' -t 10
+done
