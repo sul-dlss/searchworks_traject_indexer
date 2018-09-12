c04306f3b17090a68e44485d7d805a0d0c9b8f97
Chris Beer <cabeer@stanford.edu>
2018-09-12 16:59:23 -0700

    Up the RAM for traject indexing sdr objects

--- script/index_sdr.sh
+++ script/index_sdr.sh
@@ -9,7 +9,7 @@ export NUM_THREADS=8
 flock -n 200
 read -r LAST_DATE <$STATE_FILE
 
-export JRUBY_OPTS="-J-Xmx1200m"
+export JRUBY_OPTS="-J-Xmx8192m"
 bundle exec traject -c ./lib/traject/config/sdr_config.rb -s purl_fetcher.first_modified="${LAST_DATE}" -s solr_writer.max_skipped=-1 /dev/null
 
 echo $CURRENT_DATE > $STATE_FILE
