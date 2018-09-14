804e4cccdc38bd29d840e604f4ce702dbf9f2400
Chris Beer <cabeer@stanford.edu>
2018-09-14 12:22:41 -0700

    Wrap SDR indexing so it runs under MRI ruby until the nokogiri memory leak is plugged

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,12 @@
 set :output, 'log/cron.log'
 
 job_type :honeybadger_wrapped_script,  "cd :path && :environment_variable=:environment bundle exec honeybadger exec -q script/:task :output"
+job_type :honeybadger_wrapped_mri_ruby_script, "cd :path && :environment_variable=:environment /usr/local/rvm/bin/rvm ruby-2.4.4 do  bundle exec honeybadger exec -q script/:task :output"
 
 # index + delete SDR
 every '*/15 * * * *' do
-  script 'index_sdr.sh'
-  script 'delete_sdr.sh'
+  honeybadger_wrapped_mri_ruby_script 'index_sdr.sh'
+  honeybadger_wrapped_mri_ruby_script 'delete_sdr.sh'
 end
 
 every '45 6-23 * * *' do
