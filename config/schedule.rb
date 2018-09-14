22128a93349527f7426b08a8bb5472fdc23df192
Shelley Doljack <sdoljack@stanford.edu>
2018-09-14 15:38:38 -0700

    Puts env vars after bash script.

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,16 +8,16 @@ every '*/15 * * * *' do
 end
 
 every '45 6-23 * * *' do
-  honeybadger_wrapped_script 'SIRSI_SERVER=morison SOLR_URL=${MORISON_SOLR_URL} index_sirsi_hourly.sh'
-  honeybadger_wrapped_script 'SIRSI_SERVER=bodoni index_sirsi_hourly.sh'
+  honeybadger_wrapped_script 'index_sirsi_hourly.sh SIRSI_SERVER=morison SOLR_URL=${MORISON_SOLR_URL}'
+  honeybadger_wrapped_script 'index_sirsi_hourly.sh SIRSI_SERVER=bodoni'
 end
 
 every :day, at: '4:30am' do
-  honeybadger_wrapped_script 'SIRSI_SERVER=morison SOLR_URL=${MORISON_SOLR_URL} index_sirsi_nightly.sh'
-  honeybadger_wrapped_script 'SIRSI_SERVER=bodoni index_sirsi_nightly.sh'
+  honeybadger_wrapped_script 'index_sirsi_nightly.sh SIRSI_SERVER=morison SOLR_URL=${MORISON_SOLR_URL}'
+  honeybadger_wrapped_script 'index_sirsi_nightly.sh SIRSI_SERVER=bodoni'
 end
 
 every :day, at: '1:00am' do
-  honeybadger_wrapped_script 'SIRSI_SERVER=morison SOLR_URL=${MORISON_SOLR_URL} index_sirsi_full.sh new'
-  honeybadger_wrapped_script 'SIRSI_SERVER=bodoni index_sirsi_full.sh new'
+  honeybadger_wrapped_script 'index_sirsi_full.sh new SIRSI_SERVER=morison SOLR_URL=${MORISON_SOLR_URL}'
+  honeybadger_wrapped_script 'index_sirsi_full.sh new SIRSI_SERVER=bodoni'
 end
