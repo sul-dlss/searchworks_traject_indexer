5404de41c7053c186adc2ea8999e07990116ec24
Jack Reed <phillipjreed@gmail.com>
2018-10-25 11:41:53 -0600

    remove unused dev env

--- config/deploy/dev.rb
+++
@@ -1,5 +0,0 @@
-server 'sw-indexing-dev.stanford.edu', user: 'harvestdor', roles: %w(app)
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
-set :deploy_to, "/opt/app/harvestdor/searchworks_traject_indexer"
