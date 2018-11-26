83e2fed566baee78f047e2ae66cd9206fd07f7ac
Chris Beer <cabeer@stanford.edu>
2018-11-26 15:29:19 -0800

    Extract holdings helper to ensure our holdings memoization is actually being used

--- lib/sirsi_holding.rb
+++ lib/sirsi_holding.rb
@@ -19,8 +19,8 @@ class SirsiHolding
                     TECHSHADOW TECH-UNIQ WEST-7B SUPERSEDE WITHDRAWN].freeze
   TEMP_CALLNUM_PREFIX = 'XX'.freeze
 
-  attr_reader :current_location, :home_location, :library, :scheme, :type, :barcode
-  def initialize(call_number: '', current_location: '', home_location: '', library: '', scheme: '', type: '', barcode: '')
+  attr_reader :current_location, :home_location, :library, :scheme, :type, :barcode, :tag
+  def initialize(tag: nil, call_number: '', current_location: '', home_location: '', library: '', scheme: '', type: '', barcode: '')
     @call_number = call_number
     @current_location = current_location
     @home_location = home_location
@@ -28,6 +28,7 @@ class SirsiHolding
     @scheme = scheme
     @type = type
     @barcode = barcode
+    @tag = tag
   end
 
   def call_number
