5e2178bfc7b2f174a618eb37c8d0668c164af882
Chris Beer <cabeer@stanford.edu>
2018-11-29 14:42:06 -0800

    Fix sdr config logging

--- lib/traject/config/sdr_config.rb
+++ lib/traject/config/sdr_config.rb
@@ -386,5 +386,5 @@ each_record do |record, context|
   t0 = context.clipboard[:benchmark_start_time]
   t1 = Time.now
 
-  logger.debug('sirsi_config.rb') { "Processed #{context.source_record_id} (#{t1 - t0}s)" }
+  logger.debug('sdr_config.rb') { "Processed #{context.output_hash['id']} (#{t1 - t0}s)" }
 end
