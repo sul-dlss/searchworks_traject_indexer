918903ff2c7d788fba43219c0ee3b8522f73a3ea
Chris Beer <cabeer@stanford.edu>
2019-09-05 17:11:53 -0700

    Add a handy reader that takes in a list of druids and tries to index them

---
+++ spec/lib/traject/readers/druid_reader_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Traject::DruidReader do
+  subject(:reader) { described_class.new("a\nb\nc", settings) }
+  let(:settings) { {} }
+
+  describe '#each' do
+    it 'returns objects from the purl-fetcher api' do
+      expect(reader.each.map(&:druid)).to eq %w[a b c]
+    end
+  end
+end
