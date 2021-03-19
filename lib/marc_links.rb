1352cfbcc9cb43e5a1f0d356a89b3aa398b2768d
Chris Beer <chris@cbeer.info>
2021-03-19 10:30:28 -0700

    Refactor marclinks parsing and expose the individual parts

--- lib/marc_links.rb
+++ lib/marc_links.rb
@@ -4,156 +4,146 @@ module MarcLinks
   class Processor
     attr_reader :link_field
 
+    def field
+      link_field
+    end
+
     def initialize(link_field)
       @link_field = link_field
     end
+
     def as_h
-      link = process_link(link_field)
-      if link
-        {
-          html: ["<a title='#{link[:title]}' href='#{link[:href]}'>#{link[:text]}</a>", "#{'(source: Casalini)' if link[:casalini_toc]}", (" #{link[:additional_text]}" if link[:additional_text])].compact.join(' '),
-          text: [link[:text], "#{'(source: Casalini)' if link[:casalini_toc]}", " #{link[:additional_text] if link[:additional_text]}"].compact.join(' ').strip,
-          href: link[:href],
-          fulltext: link_is_fulltext?(link_field),
-          stanford_only: stanford_only?(link),
-          finding_aid: link_is_finding_aid?(link_field),
-          managed_purl: link_is_managed_purl?(link),
-          file_id: file_id(link_field),
-          druid: druid(link),
-          sort: link[:sort],
-          sfx: link_is_sfx?(link_field)
-        }
-      end
+      return unless link_host
+
+      {
+        version: '0.1',
+
+        html: ["<a title='#{link_title}' href='#{link_field["u"]}'>#{link_text}</a>", "#{'(source: Casalini)' if link_is_casalini?}", (" <span class='additional-link-text'>#{additional_text}</span>" if additional_text)].compact.join(' '),
+        text: [link_text, "#{'(source: Casalini)' if link_is_casalini?}", (" <span class='additional-link-text'>#{additional_text}</span>" if additional_text)].compact.join(' ').strip,
+
+        stanford_only: stanford_only?,
+        stanford_law_only: stanford_law_only?,
+
+        link_text: link_text,
+        link_title: link_title,
+        additional_text: additional_text,
+        href: link_field["u"],
+        sort: purl_info['sort'],
+        casalini: link_is_casalini?,
+
+        fulltext: link_is_fulltext?,
+        finding_aid: link_is_finding_aid?,
+        managed_purl: link_is_managed_purl?,
+        file_id: purl_info['file'],
+        druid: druid,
+        sfx: link_is_sfx?
+      }
     end
 
     private
 
-    def link_is_sfx?(link_field)
+    def link_is_casalini?
+      field["x"] and field["x"] == "CasaliniTOC"
+    end
+
+    def link_is_sfx?
       link_field['u']&.match? Regexp.union(%r{^http://library.stanford.edu/sfx\?.+}, %r{^http://caslon.stanford.edu:3210/sfxlcl3\?.+})
     end
 
-    def file_id(link_field)
-      return unless link_field['x']
-      subxs = link_field.subfields.select do |subfield|
-        subfield.code == 'x'
-      end
-
-      file_id_value = subxs.find do |subx|
-        subx.value.start_with?('file:')
-      end&.value
-
-      file_id_value.gsub('file:', '') if file_id_value
-    end
-
     # Parse a URI object to return the host of the URL in the "url" parameter if it's a proxied resoruce
-    def link_host(link)
-      return link.host unless link.to_s =~ PROXY_REGEX && link.to_s.include?('url=')
-      proxy = CGI.parse(link.query.force_encoding(Encoding::UTF_8))
-      return link.host unless proxy.key?('url')
+    def link_host
+      return if field['u'].nil?
 
-      extracted_url = URI.extract(proxy['url'].first).first
-      return link.host unless extracted_url
-      URI.parse(extracted_url).host
-    end
-
-    def process_link(field)
-      unless field['u'].nil?
+      @link_host ||= begin
         # Not sure why I need this, but it fails on certain URLs w/o it.  The link printed still has character in it
         fixed_url = field['u'].gsub("^","").strip
-        url = URI.parse(fixed_url)
-        sub3 = nil
-        subz = []
-        suby = nil
-        field.each{|subfield|
-          if subfield.code == "3"
-            sub3 = subfield.value
-          elsif subfield.code == "z"
-            subz << subfield.value
-          elsif subfield.code == "y"
-            suby = subfield.value
-          end
-        }
+        link = URI.parse(fixed_url)
 
-        if field["x"] and field["x"] == "CasaliniTOC"
-          {:text=>field["3"],
-           :title=>"",
-           :href=>field["u"],
-           :casalini_toc => true,
-           :managed_purl => (field["u"] && field['x'] =~ /SDR-PURL/)
-          }
-        elsif field['x'] && field['x'] =~ /SDR-PURL/
-          subxes = field.subfields.select { |subfield| subfield.code == 'x' }.map { |subfield| subfield.value.split(':', 2).map(&:strip) }.select { |x| x.length == 2 }.to_h
+        return link.host unless link.to_s =~ PROXY_REGEX && link.to_s.include?('url=')
+        proxy = CGI.parse(link.query.force_encoding(Encoding::UTF_8))
+        return link.host unless proxy.key?('url')
 
-          link_text = subxes['label'] unless subxes['label'].nil?
-          sort = subxes['sort']
-
-          title = subz.join(' ') unless subz.empty?
-          if title =~ stanford_affiliated_regex && !(subbed_title = title.gsub(stanford_affiliated_regex, '')).empty?
-            additional_text = "<span class='additional-link-text'>#{subbed_title}</span>".html_safe
-          end
-
-          {
-            text: link_text,
-            title: title,
-            href: field['u'],
-            casalini_toc: false,
-            additional_text: additional_text,
-            sort: sort,
-            managed_purl: true,
-            file_id: subxes['file']
-          }
-        else
-          link_text = (!suby && !sub3) ? link_host(url) : [sub3, suby].compact.join(' ')
-          title = subz.join(" ")
-          additional_text = nil
-          if title =~ stanford_affiliated_regex
-            subbed_title = title.gsub(stanford_affiliated_regex, '')
-            additional_text = "<span class='additional-link-text'>#{subbed_title}</span>" unless subbed_title.empty?
-            title = "Available to Stanford-affiliated users only" unless field['x'] && field['x'] =~ /SDR-PURL/
-          elsif title =~ stanford_law_affiliated_regex
-            additional_text = "<span class='additional-link-text'>#{title}</span>"
-          end
-          {:text=>link_text,
-           :title=> title,
-           :href=>field["u"],
-           :casalini_toc => false,
-           :additional_text => additional_text
-          }
-        end
-      end
+        extracted_url = URI.extract(proxy['url'].first).first
+        return link.host unless extracted_url
+        URI.parse(extracted_url).host
       rescue URI::InvalidURIError
         return nil
+      end
     end
 
-    def link_is_fulltext?(field)
-      return !link_is_sfx?(field) if field.tag == '956'
+    def link_text
+      if field["x"] and field["x"] == "CasaliniTOC"
+        link_field['3']
+      elsif field['x'] && field['x'] =~ /SDR-PURL/
+        purl_info['label']
+      else
+        sub3 = field['3']
+        suby = field['y']
+        (!suby && !sub3) ? link_host : [sub3, suby].compact.join(' ')
+      end
+    end
+
+    def subzs
+      @subzs ||= field.select { |sf| sf.code == 'z' }.map(&:value).join(' ')
+    end
+
+    def link_title
+      return '' if field["x"] and field["x"] == "CasaliniTOC"
+
+      return subzs if field['x'] && field['x'] =~ /SDR-PURL/
+
+      if subzs =~ stanford_affiliated_regex
+        "Available to Stanford-affiliated users only"
+      else
+        subzs
+      end
+    end
+
+    def additional_text
+      return subzs if stanford_law_only?
+      return unless stanford_only?
+
+      subbed_title = subzs.gsub(stanford_affiliated_regex, '')
+      subbed_title unless subbed_title.empty?
+    end
+
+    def purl_info
+      return {} unless link_field['x'] && link_field['x'] =~ /SDR-PURL/
+
+      @purl_info ||= link_field.subfields.select { |subfield| subfield.code == 'x' }.map { |subfield| subfield.value.split(':', 2).map(&:strip) }.select { |x| x.length == 2 }.to_h
+    end
+
+    def link_is_fulltext?
+      return !link_is_sfx? if field.tag == '956'
 
       resource_labels = ["table of contents", "abstract", "description", "sample text"]
       return false unless %w[0 1].include?(field.indicator2)
 
       # Similar logic exists in the mapping for the url_fulltext field in sirsi traject config.
       # They need to remain the same (or should be refactored to use the same code in the future)
-      resource_labels.each do |resource_label|
-        return false if "#{field['3']} #{field['z']}".downcase.include?(resource_label)
+      resource_labels.none? do |resource_label|
+        "#{field['3']} #{field['z']}".downcase.include?(resource_label)
       end
-
-      true
     end
 
-    def link_is_finding_aid?(field)
+    def link_is_finding_aid?
       "#{field['3']} #{field['z']}".downcase.include?('finding aid')
     end
 
-    def stanford_only?(link)
-      [link[:text], link[:title]].join.downcase =~ stanford_affiliated_regex
+    def stanford_only?
+      subzs.match?(stanford_affiliated_regex) || "#{field['3']} #{field['z']}".match?(stanford_affiliated_regex)
     end
 
-    def link_is_managed_purl?(link)
-      link[:managed_purl]
+    def stanford_law_only?
+      subzs.match?(stanford_law_affiliated_regex)
     end
 
-    def druid(link)
-      link[:href].gsub(%r{^https?:\/\/purl.stanford.edu\/?}, '') if link[:href] =~ /purl.stanford.edu/
+    def link_is_managed_purl?
+      field["u"] && field['x'] && field['x'].match?(/SDR-PURL/)
+    end
+
+    def druid
+      field['u'] && field['u'].gsub(%r{^https?:\/\/purl.stanford.edu\/?}, '') if field['u'] =~ /purl.stanford.edu/
     end
 
     def stanford_affiliated_regex
