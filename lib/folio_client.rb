19ea1feb22459e52f27612e83c4036504ba380cc
Chris Beer <chris@cbeer.info>
2022-07-28 16:16:50 -0700

    Add a client for Folio/OKAPI requests

---
+++ lib/folio_client.rb
@@ -0,0 +1,46 @@
+require 'http'
+
+class FolioClient
+  DEFAULT_HEADERS = {
+    accept: 'application/json, text/plain',
+    content_type: 'application/json'
+  }.freeze
+
+  attr_reader :base_url
+
+  def initialize(url: ENV['OKAPI_URL'], username: ENV['OKAPI_USER'], password: ENV['OKAPI_PASSWORD'], tenant: 'sul')
+    @base_url = url
+    @username = username
+    @password = password
+    @tenant = tenant
+  end
+
+  def get(path)
+    parse(authenticated_request(path))
+  end
+
+  def parse(response)
+    JSON.parse(response)
+  end
+
+  def session_token
+    @session_token ||= begin
+      response = request('/authn/login', json: { username: @username, password: @password }, method: :post)
+      response['x-okapi-token']
+    end
+  end
+
+  def authenticated_request(path, headers: {}, **other)
+    request(path, headers: headers.merge('x-okapi-token': session_token), **other)
+  end
+
+  def request(path, headers: {}, method: :get, **other)
+    HTTP
+      .headers(default_headers.merge(headers))
+      .request(method, base_url + path, **other)
+  end
+
+  def default_headers
+    DEFAULT_HEADERS.merge({ 'X-Okapi-Tenant': @tenant, 'User-Agent': 'FolioApiClient' })
+  end
+end
