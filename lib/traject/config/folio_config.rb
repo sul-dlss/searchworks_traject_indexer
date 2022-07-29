77f06198e90597ef47727d60abc17abbdeec836c
Nick Budak <budak@stanford.edu>
2022-07-29 11:27:45 -0700

    Port major title and text fields from sirsi to folio config

--- lib/traject/config/folio_config.rb
+++ lib/traject/config/folio_config.rb
@@ -1,18 +1,28 @@
 $LOAD_PATH << File.expand_path('../..', __dir__)
 
 require 'traject'
+require 'traject/macros/marc21_semantics'
 require 'traject/readers/folio_reader'
 require 'traject/writers/solr_better_json_writer'
+require 'traject/common/marc_utils'
 require 'i18n'
 require 'honeybadger'
 require 'utils'
 
 I18n.available_locales = [:en]
 
+extend Traject::Macros::Marc21
+extend Traject::Macros::Marc21Semantics
 extend Traject::SolrBetterJsonWriter::IndexerPatch
+extend Traject::MarcUtils
 
 Utils.logger = logger
 indexer = self
+
+ALPHABET = [*'a'..'z'].join('')
+A_X = ALPHABET.slice(0, 24)
+MAX_CODE_POINT = 0x10FFFF.chr(Encoding::UTF_8)
+CJK_RANGE = /(\p{Han}|\p{Hangul}|\p{Hiragana}|\p{Katakana})/.freeze
 
 settings do
   provide 'writer_class_name', 'Traject::SolrBetterJsonWriter'
@@ -32,11 +42,147 @@ settings do
   provide 'solr_json_writer.skippable_exceptions', [HTTPClient::TimeoutError, StandardError]
 end
 
+# Change the XMLNS to match how solrmarc handles this
+# Copied from sirsi_config.rb
+class SolrMarcStyleFastXMLWriter < MARC::FastXMLWriter
+  class << self
+    def open_collection(use_ns)
+      if use_ns
+        %Q{<collection xmlns="http://www.loc.gov/MARC21/slim">}.dup
+      else
+        "<collection>".dup
+      end
+    end
+  end
+end
+
+# Monkey-patch MarcExtractor in order to add logic to strip subfields before
+# joining them, for parity with solrmarc.
+# Copied from sirsi_config.rb
+class Traject::MarcExtractor
+  def collect_subfields(field, spec)
+      subfields = field.subfields.collect do |subfield|
+        subfield.value if spec.includes_subfield_code?(subfield.code)
+      end.compact
+
+      return subfields if subfields.empty? # empty array, just return it.
+
+      if options[:separator] && spec.joinable?
+        subfields = [subfields.map(&:strip).join(options[:separator])]
+      end
+
+      return subfields
+  end
+end
 
 to_field 'id', extract_marc('001') do |_record, accumulator|
   accumulator.map! do |v|
     v.sub(/^a/, '')
   end
+end
+
+to_field 'hashed_id_ssi' do |_record, accumulator, context|
+  next unless context.output_hash['id']
+
+  accumulator << Digest::MD5.hexdigest(context.output_hash['id'].first)
+end
+
+to_field 'marcxml' do |record, accumulator|
+  accumulator << (SolrMarcStyleFastXMLWriter.single_record_document(record.marc_record, include_namespace: true) + "\n")
+end
+
+to_field 'all_search' do |record, accumulator|
+  keep_fields = %w[024 027 028 033 905 908 920 986 979]
+  result = []
+  record.each do |field|
+    next unless (100..899).cover?(field.tag.to_i) || keep_fields.include?(field.tag)
+
+    subfield_values = field.subfields.collect(&:value)
+    next unless subfield_values.length > 0
+
+    result << subfield_values.join(' ')
+  end
+  accumulator << result.join(' ') if result.any?
+end
+
+to_field 'vern_all_search' do |record, accumulator|
+  keep_fields = %w[880]
+  result = []
+  record.each do |field|
+    next unless  keep_fields.include?(field.tag)
+    subfield_values = field.subfields
+                           .select { |sf| ALPHABET.include? sf.code }
+                           .collect(&:value)
+
+    next unless subfield_values.length > 0
+
+    result << subfield_values.join(' ')
+  end
+  accumulator << result.join(' ') if result.any?
+end
+
+# Title Search Fields
+to_field 'title_245a_search', extract_marc_and_prefer_non_alternate_scripts('245a', first: true)
+to_field 'vern_title_245a_search', extract_marc('245aa', first: true, alternate_script: :only)
+to_field 'title_245_search', extract_marc_and_prefer_non_alternate_scripts('245abfgknps', first: true)
+to_field 'vern_title_245_search', extract_marc('245abfgknps', first: true, alternate_script: :only)
+to_field 'title_uniform_search', extract_marc('130adfgklmnoprst:240adfgklmnoprs', first: true, alternate_script: false)
+to_field 'vern_title_uniform_search', extract_marc('130adfgklmnoprst:240adfgklmnoprs', first: true, alternate_script: :only)
+to_field 'title_variant_search', extract_marc('210ab:222ab:242abnp:243adfgklmnoprs:246abfgnp:247abfgnp', alternate_script: false)
+to_field 'vern_title_variant_search', extract_marc('210ab:222ab:242abnp:243adfgklmnoprs:246abfgnp:247abfgnp', alternate_script: :only)
+to_field 'title_related_search', extract_marc('505t:700fgklmnoprst:710dfgklmnoprst:711fgklnpst:730adfgklmnoprst:740anp:760st:762st:765st:767st:770st:772st:773st:774st:775st:776st:777st:780st:785st:786st:787st:796fgklmnoprst:797dfgklmnoprst:798fgklnpst:799adfgklmnoprst', alternate_script: false)
+to_field 'vern_title_related_search', extract_marc('505tt:700fgklmnoprst:710dfgklmnoprst:711fgklnpst:730adfgklmnoprst:740anp:760st:762st:765st:767st:770st:772st:773st:774st:775st:776st:777st:780st:785st:786st:787st:796fgklmnoprst:797dfgklmnoprst:798fgklnpst:799adfgklmnoprst', alternate_script: :only)
+# Title Display Fields
+to_field 'title_245a_display', extract_marc('245a', first: true, alternate_script: false) do |record, accumulator|
+  accumulator.map!(&method(:clean_facet_punctuation))
+  accumulator.map!(&method(:trim_punctuation_custom))
+end
+to_field 'vern_title_245a_display', extract_marc('245aa', first: true, alternate_script: :only) do |record, accumulator|
+  accumulator.map!(&method(:trim_punctuation_custom))
+end
+to_field 'title_245c_display', extract_marc('245c', first: true, alternate_script: false) do |record, accumulator|
+  accumulator.map!(&method(:clean_facet_punctuation))
+  accumulator.map!(&method(:trim_punctuation_custom))
+end
+to_field 'vern_title_245c_display', extract_marc('245cc', first: true, alternate_script: :only) do |record, accumulator|
+  accumulator.map!(&method(:trim_punctuation_custom))
+end
+to_field 'title_display', extract_marc('245abdefghijklmnopqrstuvwxyz', first: true, alternate_script: false) do |record, accumulator|
+  accumulator.map!(&method(:clean_facet_punctuation))
+  accumulator.map!(&method(:trim_punctuation_custom))
+end
+to_field 'vern_title_display', extract_marc('245abdefghijklmnopqrstuvwxyz', first: true, alternate_script: :only) do |record, accumulator|
+  accumulator.map!(&method(:trim_punctuation_custom))
+end
+to_field 'title_full_display', extract_marc("245#{ALPHABET}", first: true, alternate_script: false)
+to_field 'vern_title_full_display', extract_marc("245#{ALPHABET}", first: true, alternate_script: :only)
+to_field 'title_uniform_display', extract_marc(%w(130 240).map { |c| "#{c}#{ALPHABET}" }.join(':'), first: true, alternate_script: false)
+# # ? no longer will use title_uniform_display due to author-title searching needs ? 2010-11
+# TODO: Remove looks like SearchWorks is not using, confirm relevancy changes
+to_field 'vern_title_uniform_display', extract_marc(%w(130 240).map { |c| "#{c}#{ALPHABET}" }.join(':'), first: true, alternate_script: :only)
+# # Title Sort Field
+to_field 'title_sort' do |record, accumulator|
+  result = []
+  result << extract_sortable_title("130#{ALPHABET}", record)
+  result << extract_sortable_title('245abdefghijklmnopqrstuvwxyz', record)
+  str = result.join(' ').strip
+  accumulator << str unless str.empty?
+end
+
+## FOLIO specific fields
+
+## QUESTIONS
+# - change hashed_id to use uuid_ssi, since it's already a hash of some other fields?
+# - use marc JSON (marc_json_struct) instead of marcxml?
+# - what's in the 9XX fields set as keep_fields for all_search coming out of FOLIO?
+# - why did we subclass MARC::FastXMLWriter and is the behavior in SolrMarcStyleFastXMLWriter still required?
+
+to_field 'uuid_ssi' do |record, accumulator|
+  accumulator << record.instance_id
+end
+
+to_field 'marc_json_struct' do |record, accumulator|
+  accumulator << record.marc_record.to_json
 end
 
 to_field 'folio_json_struct' do |record, accumulator|
