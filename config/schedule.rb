6e09e2d166c1db8d33e5e750afdfed9d641bd438
Chris Beer <cabeer@stanford.edu>
2022-08-02 17:16:15 -0700

    Run folio loading on stage using ruby 3.1.2

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # and also redirect stderr to stdout to honeybadger doesn't complain
 job_type :honeybadger_wrapped_jruby_script, "cd :path && :environment_variable=:environment TRAJECT_ENV=:traject_env /usr/local/rvm/bin/rvm jruby-9.3.4.0 do bundle exec honeybadger exec -e :environment::traject_env -q script/:task"
 
-job_type :honeybadger_wrapped_ruby_script, "cd :path && :environment_variable=:environment SIRSI_SERVER=:sirsi_server PURL_FETCHER_URL=:purl_fetcher_url PURL_FETCHER_TARGET=:purl_fetcher_target OKAPI_URL=:okapi_url KAFKA_CONSUMER_GROUP_ID=:kafka_consumer_group_id KAFKA_TOPIC=:kafka_topic SOLR_URL=:solr_url /usr/local/rvm/bin/rvm default do bundle exec honeybadger exec -e :environment::sirsi_server -q script/:task"
+job_type :honeybadger_wrapped_ruby_script, "cd :path && :environment_variable=:environment SIRSI_SERVER=:sirsi_server PURL_FETCHER_URL=:purl_fetcher_url PURL_FETCHER_TARGET=:purl_fetcher_target OKAPI_URL=:okapi_url KAFKA_CONSUMER_GROUP_ID=:kafka_consumer_group_id KAFKA_TOPIC=:kafka_topic SOLR_URL=:solr_url /usr/local/rvm/bin/rvm ruby-3.1.2 do bundle exec honeybadger exec -e :environment::sirsi_server -q script/:task"
 
 # index + delete SDR
 every '* * * * *' do
@@ -40,6 +40,6 @@ every :day, at: '09:00pm' do
 end
 
 # USING FOLIO DATA
-every :hour do
+every :hour, roles: [:stage] do
   honeybadger_wrapped_ruby_script 'load_folio.sh', sirsi_server: 'folio', solr_url: 'http://sul-solr-prod.stanford.edu/solr/searchworks-folio-dev', okapi_url: 'https://okapi-dev.stanford.edu', kafka_topic: :marc_folio
 end
