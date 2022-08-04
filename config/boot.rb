12e6e1ed40033219b18408277c85229da90c90c9
Chris Beer <chris@cbeer.info>
2022-08-04 08:23:34 -0700

    Allow ENV variables to overwrite settings

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,11 @@
 $LOAD_PATH << File.expand_path('../lib', __dir__)
 require 'config'
 
+Config.setup do |config|
+  config.const_name = 'Settings'
+  config.use_env = true
+end
+
 Config.load_and_set_settings(Config.setting_files(__dir__, ENV['TRAJECT_ENV']))
 
 require 'utils'
