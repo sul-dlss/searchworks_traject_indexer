b4b4def37ca7e7645d1b4d7942d1ed29ac54eee5
Chris Beer <chris@cbeer.info>
2022-08-04 08:22:09 -0700

    Update folio->kafka data extractors to use stored settings

--- script/process_folio_to_kafka.rb
+++ script/process_folio_to_kafka.rb
@@ -1,18 +1,16 @@
-$LOAD_PATH << File.expand_path('../lib', __dir__)
-
-require 'utils'
-require 'logger'
+require_relative '../config/boot'
 
 require 'folio_client'
 require 'traject'
 require 'traject/readers/folio_reader'
 require 'traject/extractors/folio_kafka_extractor'
 
-log_file = File.expand_path("../log/process_folio_to_kafka_#{ENV['KAFKA_TOPIC']}.log", __dir__)
-Utils.logger = Logger.new(log_file)
-kafka = Kafka.new(ENV.fetch('KAFKA', 'localhost:9092').split(','), logger: Utils.logger)
+
+log_file = File.expand_path("../log/process_marc_to_kafka_#{Utils.env_config.kafka_topic}.log", __dir__)
+Utils.set_log_file(log_file)
+
 state_file = ENV['STATE_FILE'] || File.expand_path(
-  "../tmp/searchworks_traject_folio_indexer_last_run_#{ENV['KAFKA_TOPIC']}", __dir__
+  "../tmp/searchworks_traject_folio_indexer_last_run_#{Utils.env_config.kafka_topic}", __dir__
 )
 
 # Make sure there's a valid last response date to parse from the state file
@@ -24,9 +22,9 @@ File.open(state_file, 'r+') do |f|
   last_date = Time.iso8601(f.read.strip)
   Utils.logger.info "Found last_date in #{state_file}: #{last_date}"
 
-  reader = Traject::FolioReader.new(nil, 'folio.updated_after': last_date.utc.iso8601, 'folio.client': FolioClient.new(url: ENV['OKAPI_URL'], username: ENV['OKAPI_USER'], password: ENV['OKAPI_PASSWORD']))
+  reader = Traject::FolioReader.new(nil, 'folio.updated_after': last_date.utc.iso8601, 'folio.client': FolioClient.new(url: Utils.env_config.okapi_url || ENV['OKAPI_URL']))
 
-  Traject::FolioKafkaExtractor.new(reader: reader, kafka: kafka, topic: ENV['KAFKA_TOPIC']).process!
+  Traject::FolioKafkaExtractor.new(reader: reader, kafka: Utils.kafka, topic: Utils.env_config.kafka_topic).process!
 
   Utils.logger.info "Response generated at: #{reader.last_response_date} (previous: #{last_date})"
 
