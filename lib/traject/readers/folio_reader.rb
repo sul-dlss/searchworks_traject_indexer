6ef8c69a2ca83c0742c73a2a5c95c06119345f65
Nick Budak <budak@stanford.edu>
2022-08-19 11:06:33 -0700

    Set a default updatedAfter for FOLIO client

--- lib/traject/readers/folio_reader.rb
+++ lib/traject/readers/folio_reader.rb
@@ -13,7 +13,7 @@ module Traject
     def each(&block)
       return to_enum(:each) unless block_given?
 
-      response = client.get('/source-storage/stream/source-records', params: { limit: settings.fetch('source-records-limit', 2147483647).to_i, updatedAfter: settings['folio.updated_after'] })
+      response = client.get('/source-storage/stream/source-records', params: { limit: settings.fetch('source-records-limit', 2147483647).to_i, updatedAfter: settings.fetch('folio.updated_after', Time.at(0).utc.iso8601) })
       buffer = ""
       @last_response_date = Time.httpdate(response.headers['Date'])
 
