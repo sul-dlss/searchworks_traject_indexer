570e937bdfade375d1aed2411878c08d1887875c
Chris Beer <chris@cbeer.info>
2023-06-14 09:57:42 -0700

    Refactor folio_record_spec to use database-derived structures instead of mocking API calls

--- spec/lib/folio_record_spec.rb
+++ spec/lib/folio_record_spec.rb
@@ -5,23 +5,24 @@ require 'folio_record'
 require 'sirsi_holding'
 
 RSpec.describe FolioRecord do
-  subject(:folio_record) { described_class.new_from_source_record(record, client) }
+  subject(:folio_record) { described_class.new(record, client) }
   let(:client) { instance_double(FolioClient) }
   let(:record) do
     {
-      'parsedRecord' => {
-        'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d',
-        'content' => {
-          'fields' => [
-            { '001' => 'a14154194' },
-            { '918' => {
-              'subfields' => [
-                { 'a' => '14154194' }
-              ]
-            } }
-          ]
-        }
-      }
+      'instance' => {
+        'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d'
+      },
+      'holdings' => [],
+      'source_record' => [{
+        'fields' => [
+          { '001' => 'a14154194' },
+          { '918' => {
+            'subfields' => [
+              { 'a' => '14154194' }
+            ]
+          } }
+        ]
+      }]
     }
   end
 
@@ -38,102 +39,45 @@ RSpec.describe FolioRecord do
   describe 'the 590 field' do
     context 'when record has existing 590 in its MARC' do
       context 'when 590 has subfield a' do
-        let(:folio_record) do
-          described_class.new_from_source_record(
-            {
-              'parsedRecord' => {
-                'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d',
-                'content' => {
-                  'fields' => [
-                    { '001' => 'a14154194' },
-                    { '590' => {
-                      'subfields' => [
-                        { 'a' => 'Cataloged info about the Bound-with' }
-                      ]
-                    } }
-                  ]
-                }
-              }
+        let(:record) do
+          {
+            'instance' => {
+              'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d'
             },
-            client
-          )
+            'holdings' => [],
+            'source_record' => [{
+              'fields' => [
+                { '001' => 'a14154194' },
+                { '590' => {
+                  'subfields' => [
+                    { 'a' => 'Cataloged info about the Bound-with' }
+                  ]
+                } }
+              ]
+            }]
+          }
         end
-        it 'does not overwrite existing 590a' do
+        it 'does not overwrite existing 590s' do
           expect(folio_record.marc_record['590']['a']).to eq('Cataloged info about the Bound-with')
-        end
-      end
-      context 'when 590 has subfield c' do
-        let(:folio_record) do
-          described_class.new_from_source_record(
-            {
-              'parsedRecord' => {
-                'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d',
-                'content' => {
-                  'fields' => [
-                    { '001' => 'a14154194' },
-                    { '590' => {
-                      'subfields' => [
-                        { 'c' => 'Cataloged info about the parent id' }
-                      ]
-                    } }
-                  ]
-                }
-              }
-            }, client
-          )
-        end
-        it 'does not overwrite existing 590c' do
-          expect(folio_record.marc_record['590']['c']).to eq('Cataloged info about the parent id')
-        end
-      end
-      context 'when 590 has subfield d' do
-        let(:folio_record) do
-          described_class.new_from_source_record(
-            {
-              'parsedRecord' => {
-                'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d',
-                'content' => {
-                  'fields' => [
-                    { '001' => 'a14154194' },
-                    { '590' => {
-                      'subfields' => [
-                        { 'd' => 'Cataloged info totally unrelated to Bound-withs' }
-                      ]
-                    } }
-                  ]
-                }
-              }
-            },
-            client
-          )
-        end
-        it 'does not overwrite existing 590d' do
-          expect(folio_record.marc_record['590']['d']).to eq('Cataloged info totally unrelated to Bound-withs')
         end
       end
 
       context 'when 590 exists but is missing subfield a, c, or d, and has Bound-with parents via FOLIO APIs' do
-        let(:folio_record) do
-          described_class.new_from_source_record(
-            {
-              'parsedRecord' => {
-                'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d',
-                'content' => {
-                  'fields' => [
-                    { '001' => 'a84564' },
-                    { '590' => {
-                      'subfields' => []
-                    } }
-                  ]
-                }
-              }
+        let(:record) do
+          {
+            'instance' => {
+              'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d'
             },
-            client
-          )
-        end
-        before do
-          allow(folio_record).to receive(:bound_with_parents).and_return(
-            [
+            'holdings' => [],
+            'source_record' => [{
+              'fields' => [
+                { '001' => 'a14154194' },
+                { '590' => {
+                  'subfields' => []
+                } }
+              ]
+            }],
+            'boundWithParents' => [
               {
                 'parentInstanceId' => '134624',
                 'parentInstanceTitle' => 'Mursilis Sprachlähmung',
@@ -142,66 +86,64 @@ RSpec.describe FolioRecord do
                 'childHoldingCallNumber' => '064.8 .D191H'
               }
             ]
-          )
+          }
         end
-        it 'writes to 590a' do
-          expect(folio_record.marc_record['590']['a']).to eq('064.8 .D191H bound with Mursilis Sprachlähmung')
-        end
-        it 'writes to 590c' do
-          expect(folio_record.marc_record['590']['c']).to eq('134624 (parent record)')
-        end
-        it 'does not write to 590d' do
-          expect(folio_record.marc_record['590']['d']).to be_nil
+        it 'writes a new 590' do
+          expect(folio_record.marc_record['590'].subfields).to match_array([
+                                                                             have_attributes(code: 'a', value: '064.8 .D191H bound with Mursilis Sprachlähmung'),
+                                                                             have_attributes(code: 'c', value: '134624 (parent record)')
+                                                                           ])
         end
       end
     end
     context 'when record does not have existing 590 in its MARC' do
-      let(:folio_record) do
-        described_class.new_from_source_record(
-          {
-            'parsedRecord' => {
-              'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d',
-              'content' => {
-                'fields' => [
-                  { '001' => 'a84564' }
-                ]
-              }
-            }
+      let(:record) do
+        {
+          'instance' => {
+            'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d'
           },
-          client
-        )
-      end
-      context 'when FOLIO returns Bound-with data' do
-        before do
-          allow(folio_record).to receive(:bound_with_parents).and_return(
-            [
-              {
-                'parentInstanceId' => '134624',
-                'parentInstanceTitle' => 'Mursilis Sprachlähmung',
-                'parentItemId' => 'd1eece03-e4b6-5bd3-b6be-3d76ae8cf96d',
-                'parentItemBarcode' => '36105018739321',
-                'childHoldingCallNumber' => '064.8 .D191H'
-              }
+          'holdings' => [],
+          'source_record' => [{
+            'fields' => [
+              { '001' => 'a14154194' }
             ]
-          )
-        end
-        it 'creates a new 590 field' do
-          expect(folio_record.marc_record['590']).to be_a MARC::DataField
-        end
-        it 'writes to 590a' do
-          expect(folio_record.marc_record['590']['a']).to eq('064.8 .D191H bound with Mursilis Sprachlähmung')
-        end
-        it 'writes to 590c' do
-          expect(folio_record.marc_record['590']['c']).to eq('134624 (parent record)')
-        end
-        it 'does not write to 590d' do
-          expect(folio_record.marc_record['590']['d']).to be_nil
+          }],
+          'boundWithParents' => [
+            {
+              'parentInstanceId' => '134624',
+              'parentInstanceTitle' => 'Mursilis Sprachlähmung',
+              'parentItemId' => 'd1eece03-e4b6-5bd3-b6be-3d76ae8cf96d',
+              'parentItemBarcode' => '36105018739321',
+              'childHoldingCallNumber' => '064.8 .D191H'
+            }
+          ]
+        }
+      end
+
+      context 'when FOLIO returns Bound-with data' do
+        it 'writes a new 590' do
+          expect(folio_record.marc_record['590'].subfields).to match_array([
+                                                                             have_attributes(code: 'a', value: '064.8 .D191H bound with Mursilis Sprachlähmung'),
+                                                                             have_attributes(code: 'c', value: '134624 (parent record)')
+                                                                           ])
         end
       end
       context 'when FOLIO does not return Bound-with data' do
-        before do
-          allow(folio_record).to receive(:bound_with_parents).and_return([])
+        let(:record) do
+          {
+            'instance' => {
+              'id' => '0e050e3f-b160-5f5d-9fdb-2d49305fbb0d'
+            },
+            'holdings' => [],
+            'source_record' => [{
+              'fields' => [
+                { '001' => 'a14154194' }
+              ]
+            }],
+            'boundWithParents' => []
+          }
         end
+
         it 'does not create a new 590 field' do
           expect(folio_record.marc_record['590']).to be_nil
         end
