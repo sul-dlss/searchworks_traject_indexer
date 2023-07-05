062c37bb96f84f492565aa9f5486167205dd174d
Chris Beer <chris@cbeer.info>
2023-07-05 15:19:55 -0700

    Configure railsconfig to use a bash-friendly env_separator

--- config/boot.rb
+++ config/boot.rb
@@ -8,6 +8,7 @@ Bundler.require(:default)
 Config.setup do |config|
   config.const_name = 'Settings'
   config.use_env = true
+  config.env_separator = '__'
 end
 
 Config.load_and_set_settings(Config.setting_files(__dir__, ENV.fetch('TRAJECT_ENV', nil)))
