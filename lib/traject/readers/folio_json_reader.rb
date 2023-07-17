76360fa781410ddd04cfb1f0b5ec6faad18e3907
Justin Coyne <jcoyne@justincoyne.com>
2023-07-17 13:09:50 -0500

    Provide directions about making exports
    And re-export the whodunit.json fixture

--- lib/traject/readers/folio_json_reader.rb
+++ lib/traject/readers/folio_json_reader.rb
@@ -3,7 +3,11 @@
 require 'traject_plus'
 
 module Traject
-  # A Traject reader for processing MARC JSON from FOLIO via stdin.
+  # A Traject reader for processing JSON exports from FOLIO via stdin.
+  # @example An export can be produced by doing:
+  #  require 'traject/readers/folio_postgres_reader'
+  #  record = Traject::FolioPostgresReader.find_by_catkey('a14238203', 'postgres.url' => ENV['DATABASE_URL']
+  #  File.write("a14238203.json", JSON.pretty_generate(record.as_json))
   class FolioJsonReader < TrajectPlus::JsonReader
     def each(&)
       return to_enum(:each) unless block_given?
