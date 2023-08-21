8d7346cfe2a413257626a37f2df060eaeecac44a
Chris Beer <chris@cbeer.info>
2023-08-21 12:25:39 -0700

    Remove bodoni + morison indexing services

--- script/export_procfiles_to_systemd_prod.sh
+++ script/export_procfiles_to_systemd_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-foreman export -a traject -u indexer -e indexing.env -f Procfile.prod --root /opt/app/indexer/searchworks_traject_indexer/current --formation marc_bodoni_prod_indexer=1,marc_morison_prod_indexer=1,sdr_prod_indexer_catchup=2,sdr_preview_indexer=2,earthworks_prod_indexer=1,sdr_prod_indexer=8,folio_prod_indexer=8 systemd ~/service_templates
+foreman export -a traject -u indexer -e indexing.env -f Procfile.prod --root /opt/app/indexer/searchworks_traject_indexer/current --formation sdr_prod_indexer_catchup=2,sdr_preview_indexer=2,earthworks_prod_indexer=1,sdr_prod_indexer=8,folio_prod_indexer=8 systemd ~/service_templates
--- script/export_procfiles_to_systemd_stage.sh
+++ script/export_procfiles_to_systemd_stage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-foreman export -a traject -u indexer -e indexing.env -f Procfile.stage --root /opt/app/indexer/searchworks_traject_indexer/current --formation marc_bodoni_dev_indexer=1,marc_morison_dev_indexer=1,folio_dev_indexer=8,sw_dev_indexer=2,sw_preview_stage_indexer=2,earthworks_stage_indexer=1 systemd ~/service_templates
+foreman export -a traject -u indexer -e indexing.env -f Procfile.stage --root /opt/app/indexer/searchworks_traject_indexer/current --formation folio_dev_indexer=8,sw_dev_indexer=2,sw_preview_stage_indexer=2,earthworks_stage_indexer=1 systemd ~/service_templates
