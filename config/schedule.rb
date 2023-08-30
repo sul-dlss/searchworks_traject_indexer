28e01ccd6dd2f2052ca505f6cda82eed3b8dcdbf
Chris Beer <chris@cbeer.info>
2023-08-30 15:39:29 -0700

    Add SDR indexing to searchworks-gryphon-search for completeness

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,3 +20,7 @@ end
 every '0 3 8-14 * 0', roles: [:prod] do
   honeybadger_wrapped_ruby_script 'load_folio_postgres_full.sh', traject_env: 'folio_prod'
 end
+
+every '0 22 * * 0', roles: [:stage] do
+  honeybadger_wrapped_ruby_script 'load_folio_postgres_full.sh', traject_env: 'folio_prod'
+end
