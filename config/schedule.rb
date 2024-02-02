40c421c3fce247a4c463c78208c54e0242abb6d2
Chris Beer <cabeer@stanford.edu>
2024-02-02 11:11:08 -0800

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,17 +5,17 @@ job_type :honeybadger_wrapped_ruby_script,
          'cd :path && :environment_variable=:environment TRAJECT_ENV=:traject_env /usr/local/rvm/bin/rvm ruby-3.2.2 do bundle exec honeybadger exec -e :environment::traject_env -q script/:task'
 
 # USING FOLIO DATA
-#every '*/5 * * * *', roles: [:stage] do
-#  honeybadger_wrapped_ruby_script 'load_folio_postgres.sh', traject_env: 'folio_test'
-#end
+# every '*/5 * * * *', roles: [:stage] do
+#   honeybadger_wrapped_ruby_script 'load_folio_postgres.sh', traject_env: 'folio_test'
+# end
 
 every '*/5 * * * *', roles: [:prod] do
   honeybadger_wrapped_ruby_script 'load_folio_postgres.sh', traject_env: 'folio_prod'
 end
 
-#every '4 3 * * 6', roles: [:stage] do
-#  honeybadger_wrapped_ruby_script 'load_folio_postgres_full.sh', traject_env: 'folio_test'
-#end
+# every '4 3 * * 6', roles: [:stage] do
+#   honeybadger_wrapped_ruby_script 'load_folio_postgres_full.sh', traject_env: 'folio_test'
+# end
 
 every '4 2 * * 0', roles: [:prod] do
   honeybadger_wrapped_ruby_script 'load_folio_postgres_full.sh', traject_env: 'folio_prod'
