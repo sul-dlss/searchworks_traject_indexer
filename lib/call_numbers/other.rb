20b78feba452cb4aed8bf61eb840058b07a380c9
Chris Beer <chris@cbeer.info>
2024-02-29 15:53:23 -0800

    Extract CallNumbers::OtherShelfkey for consistency across call number types

--- lib/call_numbers/other.rb
+++ lib/call_numbers/other.rb
@@ -43,22 +43,10 @@ module CallNumbers
       lopped_call_number
     end
 
-    # shortcutting a shelfkey class as we just need the normalization/reverse methods
-    def to_shelfkey
-      [shelfkey_scheme, CallNumbers::ShelfkeyBase.pad_all_digits(call_number), CallNumbers::ShelfkeyBase.pad_all_digits(volume_info)].filter_map(&:presence).join(' ')
-    end
-
-    def to_reverse_shelfkey
-      CallNumbers::ShelfkeyBase.reverse(to_shelfkey).ljust(50, '~')
-    end
-
     private
 
-    # this transfomation only applies when generating shelfkeys
-    def shelfkey_scheme
-      return 'sudoc' if scheme == 'SUDOC'
-
-      'other'
+    def shelfkey_class
+      CallNumbers::OtherShelfkey
     end
   end
 end
---
+++ lib/call_numbers/other_shelfkey.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module CallNumbers
+  class OtherShelfkey < ShelfkeyBase
+    # shortcutting a shelfkey class as we just need the normalization/reverse methods
+    def to_shelfkey
+      [
+        shelfkey_scheme,
+        CallNumbers::ShelfkeyBase.pad_all_digits(call_number.call_number),
+        CallNumbers::ShelfkeyBase.pad_all_digits(call_number.volume_info)
+      ].filter_map(&:presence).join(' ')
+    end
+
+    def to_reverse_shelfkey
+      CallNumbers::ShelfkeyBase.reverse(to_shelfkey).ljust(50, '~')
+    end
+
+    private
+
+    # this transfomation only applies when generating shelfkeys
+    def shelfkey_scheme
+      return 'sudoc' if scheme == 'SUDOC'
+
+      'other'
+    end
+  end
+end
