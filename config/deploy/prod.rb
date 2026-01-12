7b5a26d7fb1fd2b45e994ef57d3ece077b73b1ff
Chris Beer <cabeer@stanford.edu>
2026-01-12 09:44:09 -0800

    Stop indexing into the old searchworks collection

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -29,30 +29,6 @@ set :indexers, [
       'solr.url' => 'http://sul-solr.stanford.edu/solr/earthworks-aardvark-prod'
     }
   },
-  { # Solr 8 searchworks index (replaced by folio_prod_indexer_2025 in December 2025),
-    # and retained for maybe a couple months for comparison purposes
-    key: 'folio_prod_indexer',
-    count: 2,
-    config: './lib/traject/config/folio_config.rb',
-    settings: {
-      'log.file' => 'log/traject_folio_prod_indexer.log',
-      'kafka.topic' => 'marc_folio_prod',
-      'kafka.consumer_group_id' => 'traject_folio_prod',
-      'reader_class_name' => 'Traject::KafkaFolioReader',
-      'solr.url' => 'http://sul-solr.stanford.edu/solr/searchworks-prod-previous'
-    }
-  },
-  { # Solr 8 searchworks index (replaced by sdr_prod_indexer_2025 in December 2025)
-    key: 'sdr_prod_indexer',
-    count: 1,
-    config: './lib/traject/config/sdr_config.rb',
-    settings: {
-      'log.file' => 'log/traject_sdr_folio_prod_indexer.log',
-      'kafka.topic' => 'purl_fetcher_prod',
-      'kafka.consumer_group_id' => 'traject_sdr_folio_prod',
-      'solr.url' => 'http://sul-solr.stanford.edu/solr/searchworks-prod-previous'
-    }
-  },
   {
     key: 'folio_prod_indexer_2025',
     count: 8,
