0b2bcd9cd99d5ea212ccbfeeac8bef7f8051a901
Justin Coyne <jcoyne@justincoyne.com>
2026-06-08 09:55:59 -0500

    Remove Earthworks indexing
    Earthworks has its own indexer now

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -20,22 +20,6 @@ set :indexers, [
     }
   },
   {
-    key: 'earthworks_stage_indexer',
-    count: 1,
-    config: './lib/traject/config/geo_aardvark_config.rb',
-    settings: {
-      'log.file' => 'log/traject_earthworks-stage-indexer.log',
-      'kafka.topic' => 'purl_fetcher_stage',
-      'kafka.consumer_group_id' => 'earthworks-stage-indexer',
-      'solr.url' => 'http://sul-solr.stanford.edu/solr/earthworks-aardvark-stage',
-      'purl.url' => 'https://sul-purl-stage.stanford.edu',
-      'stacks.url' => 'https://sul-stacks-stage.stanford.edu',
-      'searchworks.url' => 'https://searchworks-stage.stanford.edu',
-      'geoserver.pub_url' => 'https://earthworks-geoserver-stage-b.stanford.edu/geoserver',
-      'geoserver.stan_url' => 'https://earthworks-geoserver-stage-a.stanford.edu/geoserver'
-    }
-  },
-  {
     key: 'folio_dev_indexer',
     count: 4,
     config: './lib/traject/config/folio_config.rb',
